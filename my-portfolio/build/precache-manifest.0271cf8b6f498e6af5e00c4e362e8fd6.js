ba3ea6542b77289561d3e0344c20363387c3b869
Polomich <michpolonsky@gmail.com>
2022-02-28 21:23:29 -0500

    add gitignore

---
+++ my-portfolio/build/precache-manifest.0271cf8b6f498e6af5e00c4e362e8fd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f75aaaf8cb977be5318",
+    "url": "/static/css/main.0968e87c.chunk.css"
+  },
+  {
+    "revision": "0f75aaaf8cb977be5318",
+    "url": "/static/js/main.580241a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "835f9e2fed828c4f026b",
+    "url": "/static/js/2.f4f8d777.chunk.js"
+  },
+  {
+    "revision": "2f9daa4f47420eac4fc2f08594e47f5d",
+    "url": "/index.html"
+  }
+];
